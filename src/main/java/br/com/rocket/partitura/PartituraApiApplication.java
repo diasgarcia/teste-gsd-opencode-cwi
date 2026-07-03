9579fb071ad3b2323d250576b3282c9c3682972e
rafaeldiasgarcia <rafdgar@gmail.com>
2026-07-03 14:05:05 -0300

    chore: initialize spring boot project

---
+++ src/main/java/br/com/rocket/partitura/PartituraApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.rocket.partitura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PartituraApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PartituraApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/rocket/partitura/PartituraApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.rocket.partitura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PartituraApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
